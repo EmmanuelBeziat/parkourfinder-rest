11e749cc734589cfc84bf3314fd6345a455ab11e
Emmanuel Béziat <contact@emmanuelbeziat.com>
2018-08-22 00:10:57 +0200

    better conf

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const yamlConfig = require('node-yaml-config')
-const db = yamlConfig.load('./db.yaml')
+const config = yamlConfig.load('./config.yaml')
 
 module.exports = {
-	name: 'ParkourFinder',
+	name: config.app.name,
 	env: process.env.NODE_ENV || 'development',
-	port: process.env.PORT || 3030,
-	base_url: process.env.BASE_URL || 'https://rest.parkoufinder.com',
-	medias_url: process.env.MEDIAS_URL || 'https://medias.parkourfinder.com',
-	images_path: process.env.IMAGES_PATH || '../medias/',
+	port: process.env.PORT || config.app.port,
+	base_url: process.env.BASE_URL || config.app.url,
+	medias_url: process.env.MEDIAS_URL || config.medias.url,
+	images_path: process.env.IMAGES_PATH || config.medias.path,
 	db: {
-		host: process.env.MONGODB_HOST || db.host,
-		port: process.env.MONGODB_URI || db.port,
-		base: process.env.MONGODB_BASE || db.base,
-		user: process.env.MONGODB_USER || db.user,
-		pass: process.env.MONGODB_PASS || db.pass
+		host: process.env.MONGODB_HOST || config.db.host,
+		port: process.env.MONGODB_URI || config.db.port,
+		base: process.env.MONGODB_BASE || config.db.base,
+		user: process.env.MONGODB_USER || config.db.user,
+		pass: process.env.MONGODB_PASS || config.db.pass
 	}
 }
